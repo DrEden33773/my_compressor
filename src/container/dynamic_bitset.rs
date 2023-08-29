6fbfc040e1f019616e4ac7d032000d0e343b6470
DrEden33773 <edwardwang33773@gmail.com>
2023-08-29 22:44:56 +0800

    rename `box` to `unit`

--- src/container/dynamic_bitset.rs
+++ src/container/dynamic_bitset.rs
@@ -1,26 +1,26 @@
 #[allow(dead_code)]
 pub struct Info<'a> {
   n_bit: &'a usize,
-  n_box: &'a usize,
-  curr_i_box: &'a usize,
+  n_unit: &'a usize,
+  curr_i_unit: &'a usize,
 }
 
 pub struct InfoMut<'a> {
   n_bit: &'a mut usize,
-  n_box: &'a mut usize,
-  curr_i_box: &'a mut usize,
+  n_unit: &'a mut usize,
+  curr_i_unit: &'a mut usize,
 }
 
-type BoxType = u8;
-const BOX_BITS: usize = std::mem::size_of::<BoxType>() * 8;
+type UnitType = u8;
+const UNIT_BITS: usize = std::mem::size_of::<UnitType>() * 8;
 const USIZE_BYTES: usize = std::mem::size_of::<usize>();
 
 #[derive(Debug, Clone)]
 pub struct DynamicBitset {
-  data: Vec<BoxType>,
+  data: Vec<UnitType>,
   n_bit: usize,
-  n_box: usize,
-  curr_i_box: usize,
+  n_unit: usize,
+  curr_i_unit: usize,
 }
 
 impl From<&[u8]> for DynamicBitset {
@@ -35,12 +35,12 @@ impl From<&[u8]> for DynamicBitset {
     let mut res = Self::default();
     let InfoMut {
       n_bit,
-      n_box,
-      curr_i_box,
+      n_unit,
+      curr_i_unit,
     } = res.get_info_mut();
     *n_bit = from_le_bytes_slice(&buf[0..USIZE_BYTES]);
-    *n_box = from_le_bytes_slice(&buf[USIZE_BYTES..USIZE_BYTES * 2]);
-    *curr_i_box = from_le_bytes_slice(&buf[USIZE_BYTES * 2..USIZE_BYTES * 3]);
+    *n_unit = from_le_bytes_slice(&buf[USIZE_BYTES..USIZE_BYTES * 2]);
+    *curr_i_unit = from_le_bytes_slice(&buf[USIZE_BYTES * 2..USIZE_BYTES * 3]);
     res.data = buf[USIZE_BYTES * 3..].to_vec();
     res
   }
@@ -58,12 +58,12 @@ impl From<&DynamicBitset> for Vec<u8> {
     let mut buf = vec![];
     let Info {
       n_bit,
-      n_box,
-      curr_i_box,
+      n_unit,
+      curr_i_unit,
     } = dbs.get_info();
     buf.extend_from_slice(&into_le_bytes_arr(*n_bit));
-    buf.extend_from_slice(&into_le_bytes_arr(*n_box));
-    buf.extend_from_slice(&into_le_bytes_arr(*curr_i_box));
+    buf.extend_from_slice(&into_le_bytes_arr(*n_unit));
+    buf.extend_from_slice(&into_le_bytes_arr(*curr_i_unit));
     buf.extend_from_slice(&dbs.data);
     buf
   }
@@ -83,23 +83,23 @@ impl Eq for DynamicBitset {}
 impl DynamicBitset {
   pub fn sync_with_info(&mut self, info: &InfoMut) {
     self.n_bit = *info.n_bit;
-    self.n_box = *info.n_box;
-    self.curr_i_box = *info.curr_i_box;
+    self.n_unit = *info.n_unit;
+    self.curr_i_unit = *info.curr_i_unit;
   }
 
   pub fn get_info_mut(&mut self) -> InfoMut {
     InfoMut {
       n_bit: &mut self.n_bit,
-      n_box: &mut self.n_box,
-      curr_i_box: &mut self.curr_i_box,
+      n_unit: &mut self.n_unit,
+      curr_i_unit: &mut self.curr_i_unit,
     }
   }
 
   pub fn get_info(&self) -> Info {
     Info {
       n_bit: &self.n_bit,
-      n_box: &self.n_box,
-      curr_i_box: &self.curr_i_box,
+      n_unit: &self.n_unit,
+      curr_i_unit: &self.curr_i_unit,
     }
   }
 }
@@ -144,13 +144,13 @@ impl From<&DynamicBitset> for String {
 
 impl DynamicBitset {
   pub fn push(&mut self, value: bool) {
-    self.check_if_add_box();
+    self.check_if_add_unit();
     if value {
       // `00000000 1 00000000` like
-      self.data[self.curr_i_box] |= 1 << (self.n_bit % BOX_BITS);
+      self.data[self.curr_i_unit] |= 1 << (self.n_bit % UNIT_BITS);
     } else {
       // `11111111 0 11111111` like
-      self.data[self.curr_i_box] &= !(1 << (self.n_bit % BOX_BITS));
+      self.data[self.curr_i_unit] &= !(1 << (self.n_bit % UNIT_BITS));
     }
     self.n_bit += 1;
   }
@@ -161,68 +161,68 @@ impl DynamicBitset {
         1. Each unused bit is set to `0`
         2. Each popped bit will be reset to `0`
     */
-    self.data[self.curr_i_box] &= !(1 << (self.n_bit % BOX_BITS));
+    self.data[self.curr_i_unit] &= !(1 << (self.n_bit % UNIT_BITS));
     self.n_bit -= 1;
-    self.check_if_sub_box();
+    self.check_if_sub_unit();
   }
 
   pub fn back(&self) -> bool {
-    self.data[self.curr_i_box] & (1 << (self.n_bit % BOX_BITS)) != 0
+    self.data[self.curr_i_unit] & (1 << (self.n_bit % UNIT_BITS)) != 0
   }
 }
 
 impl DynamicBitset {
   pub fn for_each_bit(&self, mut func: impl FnMut(u8)) {
-    // 1. box <- [0..n_box - 1]
-    for i in 0..self.n_box - 1 {
-      let curr_box = self.data[i];
-      for j in 0..BOX_BITS {
-        func(curr_box & (1 << j));
+    // 1. unit <- [0..n_unit - 1]
+    for i in 0..self.n_unit - 1 {
+      let curr_unit = self.data[i];
+      for j in 0..UNIT_BITS {
+        func(curr_unit & (1 << j));
       }
     }
-    // 2. box <- [n_box - 1]
-    let last_box = self.data[self.n_box - 1];
-    for j in 0..self.n_bit % BOX_BITS {
-      func(last_box & (1 << j));
+    // 2. unit <- [n_unit - 1]
+    let last_unit = self.data[self.n_unit - 1];
+    for j in 0..self.n_bit % UNIT_BITS {
+      func(last_unit & (1 << j));
     }
   }
 
-  pub fn for_each_box(&self, mut func: impl FnMut(u8)) {
-    for i in 0..self.n_box {
+  pub fn for_each_unit(&self, mut func: impl FnMut(u8)) {
+    for i in 0..self.n_unit {
       func(self.data[i]);
     }
   }
 }
 
 impl DynamicBitset {
-  fn check_if_add_box(&mut self) {
-    let if_add_box = self.n_bit % BOX_BITS == 0 && self.n_bit > 0;
-    if !if_add_box {
+  fn check_if_add_unit(&mut self) {
+    let if_add_unit = self.n_bit % UNIT_BITS == 0 && self.n_bit > 0;
+    if !if_add_unit {
       return;
     }
     self.data.push(0);
-    self.n_box += 1;
-    self.curr_i_box += 1;
+    self.n_unit += 1;
+    self.curr_i_unit += 1;
   }
 
-  fn check_if_sub_box(&mut self) {
-    let if_sub_box = self.n_bit % BOX_BITS == 0 && self.n_bit > 1;
-    if !if_sub_box {
+  fn check_if_sub_unit(&mut self) {
+    let if_sub_unit = self.n_bit % UNIT_BITS == 0 && self.n_bit > 1;
+    if !if_sub_unit {
       return;
     }
     self.data.pop();
-    self.n_box -= 1;
-    self.curr_i_box -= 1;
+    self.n_unit -= 1;
+    self.curr_i_unit -= 1;
   }
 }
 
 impl Default for DynamicBitset {
   fn default() -> Self {
     Self {
-      data: vec![BoxType::default()],
+      data: vec![UnitType::default()],
       n_bit: 0,
-      n_box: 1,
-      curr_i_box: 0,
+      n_unit: 1,
+      curr_i_unit: 0,
     }
   }
 }
